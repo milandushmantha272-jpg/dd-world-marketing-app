a6cf7ff15549735c43b076306d45c551d89b9072
milandushmantha272-jpg <milandushmantha272@gmail.com>
2026-09-06 01:06:32 +0530

    Update DataContext.tsx

--- src/context/DataContext.tsx
+++ src/context/DataContext.tsx
@@ -917,10 +917,5 @@ export default DataContext;
 
 export const useData = () => { const context = useContext(DataContext); if (!context) throw new Error("useData must be used within a DataProvider"); return context; };
 
-export const useData = () => {
-  const context = useContext(DataContext);
-  if (!context) {
-    throw new Error("useData must be used within a DataProvider");
-  }
-  return context;
-};
+
+  
