3a6963562840ba2b6bfa1a6e6e9d3bb61cefa5ed
milandushmantha272-jpg <milandushmantha272@gmail.com>
2026-09-12 00:09:19 +0530

    feat: register native USSD bridge

--- android/app/src/main/java/com/ddworld/marketing/MainActivity.java
+++ android/app/src/main/java/com/ddworld/marketing/MainActivity.java
@@ -3,11 +3,13 @@ package com.ddworld.marketing;
 import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
 import com.ddworld.marketing.bridge.NativeGpsBridge;
+import com.ddworld.marketing.bridge.NativeUssdBridge;
 
 public class MainActivity extends BridgeActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         registerPlugin(NativeGpsBridge.class);
+        registerPlugin(NativeUssdBridge.class);
         super.onCreate(savedInstanceState);
     }
 }
