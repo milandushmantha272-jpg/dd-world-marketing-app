740a710ac0d876897fc0eeaaf5fc07662b8236d1
milandushmantha272-jpg <milandushmantha272@gmail.com>
2026-09-13 12:07:04 +0530

    security: attach Firebase ID tokens to same-origin API requests

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 import './index.css';
+import './security/authenticatedFetch';
 import { registerSW } from 'virtual:pwa-register';
 
 // Initialize PWA Service Worker for offline asset caching & background sync
