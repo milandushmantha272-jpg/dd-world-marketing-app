bcda4782000be5909c664b8524f191deda3691ea
milandushmantha272-jpg <milandushmantha272@gmail.com>
2026-09-22 08:17:30 +0530

    fix(android): import SubscriptionManager for USSD SIM routing

--- android/app/src/main/java/com/ddworld/marketing/bridge/NativeUssdBridge.kt
+++ android/app/src/main/java/com/ddworld/marketing/bridge/NativeUssdBridge.kt
@@ -7,6 +7,7 @@ import android.net.Uri
 import android.os.Build
 import android.os.Handler
 import android.os.Looper
+import android.telephony.SubscriptionManager
 import android.telephony.TelephonyManager
 import androidx.core.content.ContextCompat
 import com.getcapacitor.JSObject
